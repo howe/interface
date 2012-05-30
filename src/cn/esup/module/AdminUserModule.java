0001c07ef4058215c9b3ef51c7340812a353e725
unknown <Administrator@9E4BE9C5233D44D.(none)>
2012-05-30 22:07:25 +0800

    整理代码

--- src/cn/esup/module/AdminUserModule.java
+++ src/cn/esup/module/AdminUserModule.java
@@ -1,31 +1,27 @@
 package cn.esup.module;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.nutz.dao.pager.Pager;
 import org.nutz.ioc.loader.annotation.IocBean;
-import org.nutz.json.Json;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Param;
 import org.nutz.service.EntityService;
 
-import org.nutz.log.Log;
-import org.nutz.log.Logs;
-
 import cn.esup.bean.AdminUser;
 
 @At("/AdminUser")
-@IocBean(fields={"dao"})
-public class AdminUserModule extends EntityService<AdminUser>{
+@IocBean(fields = {"dao"})
+public class AdminUserModule extends EntityService<AdminUser> {
 
-    private static final Log log = Logs.get();
-	
+	private static final Log log = Logs.get();
+
 	@At
-	public Object list(@Param("page") int page ,@Param("rows") int rows){
+	public Object list(@Param("page") int page, @Param("rows") int rows) {
 		if (rows < 1)
 			rows = 10;
 		Pager pager = dao().createPager(page, rows);
@@ -38,39 +34,42 @@ public class AdminUserModule extends EntityService<AdminUser>{
 		map.put("list", list);
 		return map;
 	}
-	
+
 	@At
-	public boolean add(@Param("..") AdminUser obj){
-		try{
+	public boolean add(@Param("..") AdminUser obj) {
+		try {
 			dao().insert(obj);
 			return true;
-		}catch (Throwable e) {
+		}
+		catch (Throwable e) {
 			if (log.isDebugEnabled())
-				log.debug("E!!",e);
+				log.debug("E!!", e);
 			return false;
 		}
 	}
-	
+
 	@At
-	public boolean delete(@Param("..") AdminUser obj){
-		try{
+	public boolean delete(@Param("..") AdminUser obj) {
+		try {
 			dao().delete(obj);
 			return true;
-		}catch (Throwable e) {
+		}
+		catch (Throwable e) {
 			if (log.isDebugEnabled())
-				log.debug("E!!",e);
+				log.debug("E!!", e);
 			return false;
 		}
 	}
-	
+
 	@At
-	public boolean update(@Param("..") AdminUser obj){
-		try{
+	public boolean update(@Param("..") AdminUser obj) {
+		try {
 			dao().update(obj);
 			return true;
-		}catch (Throwable e) {
+		}
+		catch (Throwable e) {
 			if (log.isDebugEnabled())
-				log.debug("E!!",e);
+				log.debug("E!!", e);
 			return false;
 		}
 	}
