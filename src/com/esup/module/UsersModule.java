09854b794306cccce2dd6470df741ae3b538c9d4
qi.yang.cn@gmail.com <qi.yang.cn@gmail.com>
2012-07-20 17:29:27 +0800

    1

--- src/com/esup/module/UsersModule.java
+++ src/com/esup/module/UsersModule.java
@@ -1,28 +1,33 @@
 package com.esup.module;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.nutz.dao.Sqls;
 import org.nutz.dao.pager.Pager;
+import org.nutz.dao.sql.Sql;
+import org.nutz.dao.sql.SqlCallback;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Param;
 import org.nutz.service.EntityService;
 
-import org.nutz.log.Log;
-import org.nutz.log.Logs;
-
 import com.esup.bean.Users;
 
 @At("/Users")
-@IocBean(fields={"dao"})
-public class UsersModule extends EntityService<Users>{
+@IocBean(fields = {"dao"})
+public class UsersModule extends EntityService<Users> {
 
-    private static final Log log = Logs.get();
-	
+	private static final Log log = Logs.get();
+
 	@At
-	public Object list(@Param("page") int page ,@Param("rows") int rows){
+	public Object list(@Param("page") int page, @Param("rows") int rows) {
 		if (rows < 1)
 			rows = 10;
 		Pager pager = dao().createPager(page, rows);
@@ -35,39 +40,65 @@ public class UsersModule extends EntityService<Users>{
 		map.put("list", list);
 		return map;
 	}
-	
+
 	@At
-	public boolean add(@Param("..") Users obj){
-		try{
+	public boolean add(@Param("..") Users obj) {
+		try {
 			dao().insert(obj);
 			return true;
-		}catch (Throwable e) {
+		}
+		catch (Throwable e) {
 			if (log.isDebugEnabled())
-				log.debug("E!!",e);
+				log.debug("E!!", e);
 			return false;
 		}
 	}
-	
+
 	@At
-	public boolean delete(@Param("..") Users obj){
-		try{
+	public boolean delete(@Param("..") Users obj) {
+		try {
 			dao().delete(obj);
 			return true;
-		}catch (Throwable e) {
+		}
+		catch (Throwable e) {
 			if (log.isDebugEnabled())
-				log.debug("E!!",e);
+				log.debug("E!!", e);
 			return false;
 		}
 	}
-	
+
 	@At
-	public boolean update(@Param("..") Users obj){
-		try{
+	public boolean update(@Param("..") Users obj) {
+		try {
 			dao().update(obj);
 			return true;
-		}catch (Throwable e) {
+		}
+		catch (Throwable e) {
 			if (log.isDebugEnabled())
-				log.debug("E!!",e);
+				log.debug("E!!", e);
+			return false;
+		}
+	}
+
+	/** 通过账户查询用户 */
+	@At
+	public boolean queryUserByAccount(String account) {
+		Sql sql = Sqls.create("select count(t.user_id) num from es_users t where t.user_name = @name");
+		sql.params().set("name", account);
+
+		sql.setCallback(new SqlCallback() {
+			public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
+				rs.next();
+				return rs.getInt("num");
+			}
+		});
+		dao().execute(sql);
+
+		int i = sql.getInt();
+
+		if (i == 1) {
+			return true;
+		} else {
 			return false;
 		}
 	}
